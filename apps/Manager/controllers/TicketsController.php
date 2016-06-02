17472ed197662672ca4a54a5fc0be06148183e06
Hudson Nicoletti <hudson19962009@gmail.com>
2016-06-02 11:47:40 -0300

    created tickets page

---
+++ apps/Manager/controllers/TicketsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Manager\Controllers;
+
+use Manager\Models\Logs as Logs,
+    Manager\Models\Tickets as Tickets;
+
+class TicketsController extends ControllerBase
+{
+
+    public function IndexAction()
+    {
+
+
+    }
+}
